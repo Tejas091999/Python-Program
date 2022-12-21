5a71bf3e48143a42c04163a84e0bcab81b974f0e
Tejas091999 <tejaslondhe9@gmail.com>
2022-12-21 17:28:21 +0530

    Add files via upload

---
+++ Fibonacci Series/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,11 @@
+print("WELCOME TO GUITAR TUTORIAL!!")
+chords=["A","B","C","D","E","F","G"]
+no_1="first_finger"
+no_2="middle_finger"
+no_3="third_finger"
+no_4="fourth_finger"
+a="second_string"
+d="third_string"
+user=input("Which chord do you want learn: ").upper()
+if user=="A":
+    print(f"2 freth ,put {no_3} on {a}\n put {no_2} on {d}")
