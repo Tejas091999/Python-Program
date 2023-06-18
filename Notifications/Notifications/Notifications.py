aed36142c64493b40f6ba6d1a94af606bd979112
Tejas091999 <tejaslondhe9@gmail.com>
2023-06-18 15:19:21 +0530

    Update Notifications.py

--- Notifications/Notifications/Notifications.py
+++ Notifications/Notifications/Notifications.py
@@ -1,7 +1,7 @@
 import random 
 import time
 while True:
-    mylist = [ "Brave","Handsome","Superb","Stunning"]
+    mylist = [ "Brave","Handsome","Superb","Stunning","Brilliant"]
     motivation = random.choice(mylist)
     time.sleep(2)
     print(motivation)
