e6e820fe26465cd7c610f88efe5b95b43a471b64
Tejas091999 <tejaslondhe9@gmail.com>
2023-07-21 14:00:29 +0530

    Update Taking random number.py

--- Taking random number.py
+++ Taking random number.py
@@ -2,4 +2,4 @@ import random
 user1 = int(input("Enter the first number: "))
 user2 = int(input("Enter the second number: "))
 finalNumber = random.randrange(user1,user2,2)
-print(finalNumber)
+print(finalNumber)   
